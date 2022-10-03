1b45562b7a9dcbb885674de3293ce4a0c287cc57
晴彦 相川 <k021g1126@g.neec.ac.jp>
2022-10-03 10:58:35 +0900

    これでいいのだ

--- main.cpp
+++ main.cpp
@@ -14,15 +14,16 @@ T Min(T a, T b)
 template<>
 char Min(char a, char b)
 {
-	return printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ");
+	printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ");
+	return 0;
 }
 
 int main(void)
 {
 	printf("%d\n", Min<int>(1, 2));
 	printf("%f\n", Min<float>(1.5f, 2.5f));
-	printf("%lf\n", Min<double>(1.5l, 2.5l));
-	printf("%c\n", Min<char>(1, 2));
+	printf("%lf\n", Min<double>(1.5, 2.5));
+	printf("%c\n", Min<char>('a', 'b'));
 
 	return (0);
 }
