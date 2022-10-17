ad0560367543d46afa27b8966a1d787d0e4d6856
晴彦 相川 <k021g1126@g.neec.ac.jp>
2022-10-17 11:15:28 +0900

    一旦できたということで

--- main.cpp
+++ main.cpp
@@ -1,5 +1,69 @@
+#include <stdio.h>
+#include <Windows.h>
+#include <time.h>
+#include <functional>
+
+typedef void (*PFunc)(int*);
+
+void callback(int* waitFrame)
+{
+	srand(time(nullptr));
+}
+
 int main()
 {
+	std::function<void(PFunc,int)>setTimeout = [](PFunc p, int i)
+	{
+		i*=1000;
+		//コールバック関数呼び出す
+		Sleep(i);
+		p(&i);
+	};
+
+	int random = rand();
+
+	char string[5];
+
+	printf("「偶数」か「奇数」を入力してください\n");
+
+	scanf_s("%s", string, 5);
+	printf("結果は...\n");
+
+	PFunc p;
+	p = callback;
+	setTimeout(p, 3);
+
+	printf("ランダムで出た値は%dでした\n", random);
+
+	//偶数の場合
+	if (strcmp(string, "偶数") == 0)
+	{
+		if (random % 2 == 0)
+		{
+			printf("当たり！");
+		}
+		else
+		{
+			printf("はずれ...");
+		}
+	}
+	//奇数の場合
+	else if (strcmp(string, "奇数") == 0)
+	{
+		if (random % 2 == 1)
+		{
+			printf("当たり！");
+		}
+		else
+		{
+			printf("はずれ...");
+		}
+	}
+	//馬鹿用
+	else
+	{
+		printf("その文字の入力は受け付けてないよ");
+	}
 
 	return (0);
 }
