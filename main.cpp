d45ce9e58c36ea10d266e99b1ad833473cdabedd
晴彦 相川 <k021g1126@g.neec.ac.jp>
2022-10-17 12:15:48 +0900

    できた

--- main.cpp
+++ main.cpp
@@ -3,67 +3,52 @@
 #include <time.h>
 #include <functional>
 
-typedef void (*PFunc)(int*);
+typedef void (*PFunc)(int);
 
-void callback(int* waitFrame)
+void callback(int num)
 {
 	srand(time(nullptr));
+	int random = rand();
+
+	printf("ランダムで出た値は%dでした\n", random);
+
+	//判定
+	if (random % 2 == num % 2)
+	{
+		printf("当たり！");
+	}
+	else
+	{
+		printf("はずれ...");
+	}
 }
 
 int main()
 {
-	std::function<void(PFunc,int)>setTimeout = [](PFunc p, int i)
-	{
-		i*=1000;
-		//コールバック関数呼び出す
-		Sleep(i);
-		p(&i);
-	};
-
-	int random = rand();
-
-	char string[5];
-
-	printf("「偶数」か「奇数」を入力してください\n");
-
-	scanf_s("%s", string, 5);
-	printf("結果は...\n");
-
 	PFunc p;
 	p = callback;
-	setTimeout(p, 3);
+	int num = 0;
 
-	printf("ランダムで出た値は%dでした\n", random);
+	printf("「奇数」なら「1」を「偶数」なら「2」を入力してください\n");
+	scanf_s("%d", &num);
 
-	//偶数の場合
-	if (strcmp(string, "偶数") == 0)
+	std::function<void(PFunc, int)>setTimeout = [=](PFunc p, int sec)
 	{
-		if (random % 2 == 0)
-		{
-			printf("当たり！");
-		}
-		else
-		{
-			printf("はずれ...");
-		}
-	}
-	//奇数の場合
-	else if (strcmp(string, "奇数") == 0)
+		printf("結果は...\n");
+		//コールバック関数呼び出す
+		Sleep(sec *= 1000);
+		p(num);
+	};
+
+	if (num == 1 || num == 2)
 	{
-		if (random % 2 == 1)
-		{
-			printf("当たり！");
-		}
-		else
-		{
-			printf("はずれ...");
-		}
+		setTimeout(p, 3);
 	}
-	//馬鹿用
 	else
 	{
-		printf("その文字の入力は受け付けてないよ");
+		printf("その入力は受け付けてないよ");
 	}
 
 	return (0);
 }
+	
