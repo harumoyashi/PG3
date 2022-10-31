7b9de56e79650bf0734acac3bb9d88a1ee8a2f80
晴彦 相川 <k021g1126@g.neec.ac.jp>
2022-10-31 12:04:04 +0900

    listように消したり

--- main.cpp
+++ main.cpp
@@ -1,54 +1,12 @@
 #include <stdio.h>
 #include <Windows.h>
-#include <time.h>
-#include <functional>
-
-typedef void (*PFunc)(int);
-
-void callback(int num)
-{
-	srand(time(nullptr));
-	int random = rand() % 6 + 1;
-
-	printf("さいころの出目は%dでした\n", random);
-
-	//判定
-	if (random % 2 == num % 2)
-	{
-		printf("当たり！");
-	}
-	else
-	{
-		printf("はずれ...");
-	}
-}
+#include <vector>
+#include <list>
 
 int main()
 {
-	PFunc p;
-	p = callback;
-	int num = 0;
-
-	printf("「半」なら「1」を「丁」なら「2」を入力してください\n");
-	scanf_s("%d", &num);
-
-	std::function<void(PFunc, int)>setTimeout = [=](PFunc p, int sec)
-	{
-		printf("結果は...\n");
-		//コールバック関数呼び出す
-		Sleep(sec *= 1000);
-		p(num);
-	};
-
-	if (num == 1 || num == 2)
-	{
-		setTimeout(p, 3);
-	}
-	else
-	{
-		printf("その入力は受け付けてないよ");
-	}
-
+	std::list<char>list;
+	
 	return (0);
 }
 	
